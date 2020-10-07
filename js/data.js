78e538d9e539540c68b3a22905b481bc8d7d8ce5
Martha Norrick <martharuthnorrick@gmail.com>
2020-10-07 17:31:59 -0400

    initial import

---
+++ js/data.js
@@ -0,0 +1,7 @@
+var beerdata = [
+  {
+    "brewery": "359 Henry Street",
+    "longitude": -73.9989742,
+    "latitude": 40.6890918,
+  }
+]
---
+++ js/scripts.js
@@ -0,0 +1,85 @@
+// this is my mapboxGL token
+// the base style includes data provided by mapbox, this links the requests to my account
+mapboxgl.accessToken = 'pk.eyJ1IjoidGhhcm1hMyIsImEiOiJjamkzazRtd3AyNWFyM2twZGpmNWp5Znh3In0.t0f4CwdP5o0wMM6adrU4Cg';
+
+// we want to return to this point and zoom level after the user interacts
+// with the map, so store them in variables
+var initialCenterPoint = [-73.986149,40.683472]
+var initialZoom = 13
+
+//add pumpkin emoji
+
+
+// create an object to hold the initialization options for a mapboxGL map
+var initOptions = {
+  container: 'map-container', // put the map in this container
+  style: 'mapbox://styles/tharma3/ckfx7qfkn16k91bnztg7luoyx', // use this basemap
+  center: initialCenterPoint, // initial view center
+  zoom: initialZoom, // initial view zoom level (0-18)
+}
+
+// create the new map
+var map = new mapboxgl.Map(initOptions);
+
+// add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
+// make a single marker in central park
+ // new mapboxgl.Marker()
+ //   .setLngLat([-73.974087,40.770718])
+ //   .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+ //    .setHTML('I am in Central Park'))
+ //   .addTo(map);
+
+
+// iterate over each object in studentData
+beerdata.forEach(function(beerdata) {
+  // for each object in the beerdata, add a marker to the map with a popup
+  var el = document.createElement('div');
+  el.className = 'marker';
+
+  new mapboxgl.Marker(el)
+    .setLngLat([beerdata.longitude, beerdata.latitude])
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML(`${beerdata.brewery}`))
+    .addTo(map);
+})
+
+// event listeners for the fly to buttons
+
+// $('#china').on('click', function() {
+//   map.flyTo({
+//     center: [112.556005, 37.818758],
+//     zoom: initialZoom
+//   })
+// })
+//
+// $('#michigan').on('click', function() {
+//
+//   var michiganLngLat = [-83.10538, 42.50448]
+//
+//   map.flyTo({
+//     center: michiganLngLat,
+//     zoom: initialZoom
+//   })
+// })
+//
+// $('#colombia').on('click', function() {
+//   var colombiaLngLat = [-73.997208, 0.721615]
+//
+//   map.flyTo({
+//     center: colombiaLngLat,
+//     zoom: initialZoom
+//   })
+// })
+//
+// $('#nyc').on('click', function() {
+//   map.flyTo({
+//     center: initialCenterPoint,
+//     zoom: initialZoom
+//   })
+// })
+
+//add search box??
+//halloweenpumpkinhunt@gmail
+//dudew33l 1birdnest
